90ff7f34df8a2ad5fc8574b789846e1d65259101
sqelder <150970682+sqelder@users.noreply.github.com>
2023-11-30 18:52:50 -0800

    Add files via upload

---
+++ post-auto.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git clone https://github.com/sqelder/sway.git
+mv ~/sway/* ~/
+rm -rf README.md
+rm -rf LICENSE
+rm -rf .git
+chmod +x ~/.local/bin/*
+chmod +x ~/.local/share/applications/*
+chmod +x ~/.config/sway/start-bar.sh
+chmod +x ~/.config/sway/start-pipewire.sh
+chmod +x ~/.config/yambar/sway-switch-keyboard.sh
+chmod +x ~/.config/yambar/xkb-layout.sh
+ln -s ~/.config/mimeapps.list ~/.local/share/applications/
